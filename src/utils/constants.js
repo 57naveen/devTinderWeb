b452e64535c8127f60e2eebd0d76c5551afb348d
57naveen <naveenkishore65@gmail.com>
2025-07-01 15:42:40 +0530

    api updated

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,6 @@
-export const BASE_URL = location.hostname === "localhost" ? "http://localhost:5555" : "/api"
+// export const BASE_URL = location.hostname === "localhost" ? "http://localhost:5555" : "/api"
+
+export const BASE_URL =
+  location.hostname === "localhost"
+    ? "http://localhost:5555"
+    : "https://devtinder-backend-mosg.onrender.com";
