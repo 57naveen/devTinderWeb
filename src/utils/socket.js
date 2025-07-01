df5f46c4807fa61ed40afb0c412ef8980e221d51
57naveen <naveenkishore65@gmail.com>
2025-07-01 17:06:41 +0530

    socket url updates

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -2,13 +2,16 @@ import io from "socket.io-client"
 
 import { BASE_URL } from "./constants"
 
-export const createSocketConnection = () =>{
-
-  if(location.hostname === "localhost"){
-    return io(BASE_URL)
+export const createSocketConnection = () => {
+  if (location.hostname === "localhost") {
+    return io(BASE_URL, {
+      withCredentials: true,
+      transports: ["websocket", "polling"]
+    });
+  } else {
+    return io("https://devtinder-backend-mosg.onrender.com", {
+      withCredentials: true,
+      transports: ["websocket", "polling"]
+    });
   }
-  else{
-    return io("/",{path:"/api/socket.io"})
-  }
- 
-}
+};
