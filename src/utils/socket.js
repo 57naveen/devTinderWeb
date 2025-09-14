acca7633b1a4ed1dcc8ba9c17f8f52761468f887
57naveen <naveenkishore65@gmail.com>
2025-09-14 15:29:59 +0530

    socket url update

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,17 +1,24 @@
-import io from "socket.io-client"
-
-import { BASE_URL } from "./constants"
+import io from "socket.io-client";
+import { BASE_URL } from "./constants";
 
 export const createSocketConnection = () => {
   if (location.hostname === "localhost") {
+    // Local development
     return io(BASE_URL, {
       withCredentials: true,
-      transports: ["websocket", "polling"]
+      transports: ["websocket", "polling"],
+    });
+  } else if (location.hostname === "16.170.231.228") {
+    // AWS deployment
+    return io("http://16.170.231.228/api", {
+      withCredentials: true,
+      transports: ["websocket", "polling"],
     });
   } else {
+    // Fallback: Render deployment
     return io("https://devtinder-backend-mosg.onrender.com", {
       withCredentials: true,
-      transports: ["websocket", "polling"]
+      transports: ["websocket", "polling"],
     });
   }
 };
